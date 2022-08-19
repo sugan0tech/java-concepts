cb1532b5d7eaa0c7b93f483923d093902147109c
sugan0tech <sugankpms@gmail.com>
2022-08-19 11:57:15 +0530

    initial commit

---
+++ test/Test.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Test {
+    public static void main(String args[]) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println(scn.nextLine());
+    }
+
+}
