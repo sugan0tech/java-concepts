1a183a2ad8bbcc1cfca7085de000eab97ebc8263
sugan0tech <sugankpms@gmail.com>
2022-08-30 19:00:20 +0530

    added collections

---
+++ TestCollections.java
@@ -0,0 +1,8 @@
+import collection.*;
+
+public class TestCollections {
+    public static void main(String args[]) {
+        collection.ArrayListTest.main(args);
+    }
+
+}
--- mypack/Main.java
+++ mypack/Main.java
@@ -1,16 +1,14 @@
 package mypack;
 
 // importing package classes
-import mypack.Default;
 import mypack.Protected;
-import mypack.Private; // only accessible within class
+// import mypack.Private; // only accessible within class
 import mypack.Default; // only accessible withing package
 
 public class Main {
     public static void main(String args[]) {
         Default.def();
         System.out.println("From Main package " + new Protected().value);
-
     }
 
 }
